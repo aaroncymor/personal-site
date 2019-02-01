eeb8c4fece29182007092c7397fa31dd7243c101
Aaron Cymor Nemeno <anemeno@risingtide.ph>
2019-02-01 13:01:37 +0800

    Adding blog with post list and post detail view. Modal activated for  projects.

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,24 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.views import generic, View
+
+from .models import Post
+from .forms import PostForm
 
 # Create your views here.
+
+class PostListView(generic.ListView):
+    model = Post
+    paginate_by = 10
+    template_name = 'blog/post_list.html'
+
+
+class PostDetailView(generic.DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
+
+
+class PostFormView(View):
+    form_class = PostForm
+    initial = {}
+    template_name = 'blog/post_form.html'
